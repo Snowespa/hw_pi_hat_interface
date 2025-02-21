326fe42ba5770111af7b54fac50bcb956817400f
snowespa <maxine.nicolay@gmail.com>
2025-02-21 23:51:54 +0000

    Added checksumCRC8, added sendPkt function, added the setBuzzer function

--- src/board.cpp
+++ src/board.cpp
@@ -5,8 +5,13 @@
 #include <termios.h>
 #include <unistd.h>
 
+#include <cstdint>
 #include <cstring>
+#include <ios>
 #include <iostream>
+#include <vector>
+
+#include "../include/hwPkt.hpp"
 
 Board::Board(const std::string &device, int baud_rate, int timeout)
     : dev(device), br(baud_rate), timeout(timeout), fd(-1) {
@@ -67,4 +72,45 @@ void Board::closePort() {
     close(fd);
     fd = -1;
   }
+}
+
+unsigned char Board::checksumCRC8(const std::vector<unsigned char> &data) {
+  unsigned char chk = 0;
+  for (auto d : data) {
+    chk = CRC8_TABLE[chk ^ d];
+  }
+  return chk;
+}
+
+void Board::sendPkt(unsigned char func,
+                    const std::vector<unsigned char> &data) {
+  std::vector<unsigned char> buf{0xAA, 0x55, func};
+  buf.push_back(static_cast<unsigned char>(data.size()));
+  buf.insert(buf.end(), data.begin(), data.end());
+  unsigned char crc8 =
+      checksumCRC8(std::vector<unsigned char>(buf.begin() + 2, buf.end()));
+  buf.push_back(crc8);
+  write(fd, buf.data(), sizeof(buf));
+}
+
+void Board::setBuzzer(const float on_time, const float off_time,
+                      const uint16_t freq, const uint16_t repeat) {
+  uint16_t on_t = static_cast<uint16_t>(on_time * 1000);
+  uint16_t off_t = static_cast<uint16_t>(off_time * 1000);
+
+  std::vector<uint8_t> data = {
+      static_cast<uint8_t>(freq & 0x00FF),
+      static_cast<uint8_t>((freq & 0xFF00) >> 8),
+      static_cast<uint8_t>(on_t & 0x00FF),
+      static_cast<uint8_t>((on_t & 0xFF00) >> 8),
+      static_cast<uint8_t>(off_t & 0x00FF),
+      static_cast<uint8_t>((off_t & 0xFF00) >> 8),
+      static_cast<uint8_t>(repeat & 0x00FF),
+      static_cast<uint8_t>((repeat & 0xFF00) >> 8),
+  };
+  for (auto d : data) {
+    std::cout << std::hex << unsigned(d) << " ";
+  }
+  std::cout << std::endl;
+  sendPkt(static_cast<uint8_t>(PktFunc::BUZ), data);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,7 @@
 int main(int argc, char *argv[]) {
   Board board("/dev/ttyAMA0", B1000000, 500);
 
+  board.setBuzzer(1.0, 0.1, 1000, 3);
+
   return 0;
 }
